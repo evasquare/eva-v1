f86deff7b76f8cc3aba4b503a16f62e7d29751c2
pixelbracket <177620651+pixelbracket@users.noreply.github.com>
2024-10-15 19:53:26 +0900

    Format song title

--- template-parts/list-view/song.php
+++ template-parts/list-view/song.php
@@ -6,7 +6,12 @@
 
 <article>
     <a class="no-decoration" href="<?php the_permalink(); ?>">
-        <h2 class="list-article-title"><?php the_title(); ?></h1>
+
+        <?php if (strtotime(get_the_title())): ?>
+            <h2 class="list-article-title"><?php echo date("F j, Y", strtotime(get_the_title())); ?></h2>
+        <?php else: ?>
+            <h2 class="list-article-title"><?php the_title(); ?></h1>
+            <?php endif; ?>
             <hr>
     </a>
 </article>
