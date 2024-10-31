ca08fda9a29154c02c8ba8cf5d8d544ced3880b3
pixelbracket <177620651+pixelbracket@users.noreply.github.com>
2024-10-31 18:02:31 +0900

    Refactor parsing code

--- template-parts/content/content-song.php
+++ template-parts/content/content-song.php
@@ -29,10 +29,13 @@ class Song
 $input = get_the_content();
 
 $previous_indent_level = 0;
-$previous_title = "";
+$previous_name = "";
 $output_array = array();
 
 foreach (explode("\n", $input) as $line) {
+    if (!$is_successful) {
+        break;
+    }
     if ($line == "") {
         continue;
     }
@@ -55,42 +58,32 @@ foreach (explode("\n", $input) as $line) {
     if (str_starts_with($new_title, "https://")) {
         if ($new_indent_level <= $previous_indent_level) {
             $is_successful = false;
-        } else {
-            if (
-                str_starts_with($new_title, "https://youtu.be/") ||
-                str_starts_with($new_title, "https://www.youtube.com/live/")
-            ) {
-                $processed_url = str_replace("https://youtu.be/", "", $new_title);
-                $processed_url = str_replace("https://www.youtube.com/live/", "", $processed_url);
+        }
 
-                array_push(
-                    $output_array,
-                    new Song(
-                        $new_indent_level - 1,
-                        $previous_title,
-                        $processed_url,
-                        false
-                    )
-                );
-            } else if (str_starts_with($new_title, 'https://youtube.com/playlist?list=')) {
-                $processed_url = str_replace("https://youtube.com/playlist?list=", "", $new_title);
+        $is_playlist = false;
+        $processed_url = null;
 
-                array_push(
-                    $output_array,
-                    new Song(
-                        $new_indent_level - 1,
-                        $previous_title,
-                        $processed_url,
-                        true
-                    )
-                );
-            }
+        if (str_starts_with($new_title, "https://youtu.be/")) {
+            $processed_url = str_replace("https://youtu.be/", "", $new_title);
+        } elseif (str_starts_with($new_title, "https://www.youtube.com/live/")) {
+            $processed_url = str_replace("https://www.youtube.com/live/", "", $new_title);
+        } elseif (str_starts_with($new_title, "https://youtube.com/playlist?list=")) {
+            $processed_url = str_replace("https://youtube.com/playlist?list=", "", $new_title);
+            $is_playlist = true;
+        }
+
+        if ($processed_url) {
+            $output_array[] = new Song(
+                $new_indent_level - 1,
+                $previous_name,
+                $processed_url,
+                $is_playlist
+            );
         }
     }
 
-
     $previous_indent_level = $new_indent_level;
-    $previous_title = $new_title;
+    $previous_name = $new_title;
 }
 ?>
 
@@ -108,24 +101,24 @@ foreach (explode("\n", $input) as $line) {
         <?php
         if ($is_successful) {
             foreach ($output_array as $song) {
-                $song_level = $song->heading_level + 2;
-                $song_title = $song->name;
-                $song_url = $song->url;
-                $song_is_playlist = $song->is_playlist;
+                $media_heading_level = $song->heading_level + 2;
+                $media_name = $song->name;
+                $media_url = $song->url;
+                $media_is_playlist = $song->is_playlist;
 
-                if ($song_is_playlist) {
+                if ($media_is_playlist) {
                     $output_string = <<<MARKER
-                    <h$song_level class="wp-block-heading">$song_title</h$song_level>
+                    <h$media_heading_level class="wp-block-heading">$media_name</h$media_heading_level>
                 
-                    <iframe title="$song_title"
-                    width="500" height="281" src="https://www.youtube.com/embed/videoseries?list=$song_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                    <iframe title="$media_name"
+                    width="500" height="281" src="https://www.youtube.com/embed/videoseries?list=$media_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
                     MARKER;
                 } else {
                     $output_string = <<<MARKER
-                    <h$song_level class="wp-block-heading">$song_title</h$song_level>
+                    <h$media_heading_level class="wp-block-heading">$media_name</h$media_heading_level>
                 
-                    <iframe title="$song_title"
-                    src="https://www.youtube.com/embed/$song_url?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                    <iframe title="$media_name"
+                    src="https://www.youtube.com/embed/$media_url?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
                     MARKER;
                 }
 
