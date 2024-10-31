3b2cd259d52c2d365d90005a0f162bd4a7af40f7
pixelbracket <177620651+pixelbracket@users.noreply.github.com>
2024-10-31 17:53:11 +0900

    Directly parse the list

--- template-parts/content/content-song.php
+++ template-parts/content/content-song.php
@@ -1,53 +1,101 @@
 <style>
-    .size-post-thumbnail {
-        object-fit: cover;
-    }
-
-    .no-image {
-        width: 100%;
-        height: auto;
-    }
-
-    .comment-form-comment {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .commentlist {
-        margin-bottom: 50px;
-    }
-
-    .form-submit input[type="submit"] {
-        border: none;
-        background: none;
-
-        padding: 15px 20px;
-        background-color: var(--accent-color);
-        color: white;
-        font-size: 14px;
-        font-weight: 500;
-        border-radius: 6px;
-
-        transition: all 0.16s ease-out;
-        transform: none;
-        cursor: pointer;
-    }
-
-    .wp-block-image img {
-        width: 100% !important;
+    h2,
+    h3,
+    h4,
+    h5 {
+        margin-top: 42px;
     }
 </style>
 
+<?php
+$is_successful = true;
+
+class Song
+{
+    var $heading_level;
+    var $name;
+    var $url;
+    var $is_playlist;
+
+    function __construct($heading_level, $title, $url, $is_playlist)
+    {
+        $this->heading_level = $heading_level;
+        $this->name = $title;
+        $this->url = $url;
+        $this->is_playlist = $is_playlist;
+    }
+}
+
+$input = get_the_content();
+
+$previous_indent_level = 0;
+$previous_title = "";
+$output_array = array();
+
+foreach (explode("\n", $input) as $line) {
+    if ($line == "") {
+        continue;
+    }
+
+    // Check tab level and text
+    $splitted_by_tabs = explode("    ", $line);
+    $new_indent_level = 0;
+    $new_title = "";
+
+    foreach ($splitted_by_tabs as $item) {
+        if ($item != "") {
+            $new_title = str_replace("- ", "", end($splitted_by_tabs));
+            break;
+        } else {
+            $new_indent_level += 1;
+        }
+    }
+
+    // Check URL
+    if (str_starts_with($new_title, "https://")) {
+        if ($new_indent_level <= $previous_indent_level) {
+            $is_successful = false;
+        } else {
+            if (
+                str_starts_with($new_title, "https://youtu.be/") ||
+                str_starts_with($new_title, "https://www.youtube.com/live/")
+            ) {
+                $processed_url = str_replace("https://youtu.be/", "", $new_title);
+                $processed_url = str_replace("https://www.youtube.com/live/", "", $processed_url);
+
+                array_push(
+                    $output_array,
+                    new Song(
+                        $new_indent_level - 1,
+                        $previous_title,
+                        $processed_url,
+                        false
+                    )
+                );
+            } else if (str_starts_with($new_title, 'https://youtube.com/playlist?list=')) {
+                $processed_url = str_replace("https://youtube.com/playlist?list=", "", $new_title);
+
+                array_push(
+                    $output_array,
+                    new Song(
+                        $new_indent_level - 1,
+                        $previous_title,
+                        $processed_url,
+                        true
+                    )
+                );
+            }
+        }
+    }
+
+
+    $previous_indent_level = $new_indent_level;
+    $previous_title = $new_title;
+}
+?>
+
 <div class="wrapper">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php if (has_post_thumbnail()): ?>
-            <?php the_post_thumbnail() ?>
-        <?php elseif (is_singular('post')): ?>
-            <img
-                class="no-image"
-                src="<?php echo get_theme_file_uri(); ?>/assets/images/no-image.png"
-                alt="No Image" />
-        <?php endif; ?>
         <?php if (strtotime(get_the_title())): ?>
             <h1 class="post-title"><?php echo date("F j, Y", strtotime(get_the_title())); ?></h1>
         <?php else: ?>
@@ -55,7 +103,36 @@
         <?php endif; ?>
 
         <?php the_tags(); ?>
-        <?php the_content(); ?>
+
+        <!-- <?php the_content(); ?> -->
+        <?php
+        if ($is_successful) {
+            foreach ($output_array as $song) {
+                $song_level = $song->heading_level + 2;
+                $song_title = $song->name;
+                $song_url = $song->url;
+                $song_is_playlist = $song->is_playlist;
+
+                if ($song_is_playlist) {
+                    $output_string = <<<MARKER
+                    <h$song_level class="wp-block-heading">$song_title</h$song_level>
+                
+                    <iframe title="$song_title"
+                    width="500" height="281" src="https://www.youtube.com/embed/videoseries?list=$song_url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                    MARKER;
+                } else {
+                    $output_string = <<<MARKER
+                    <h$song_level class="wp-block-heading">$song_title</h$song_level>
+                
+                    <iframe title="$song_title"
+                    src="https://www.youtube.com/embed/$song_url?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                    MARKER;
+                }
+
+                echo $output_string;
+            }
+        }
+        ?>
     </article>
     <!-- #post-<?php the_ID(); ?> -->
 </div>
