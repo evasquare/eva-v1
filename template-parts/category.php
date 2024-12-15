4b1ab5002d1b686910e870625b4a68656d99ed4d
evasquare <177620651+evasquare@users.noreply.github.com>
2024-12-15 15:02:25 +0900

    Use `wp_list_categories` function

--- template-parts/category.php
+++ template-parts/category.php
@@ -1,19 +1,13 @@
-<?php
-$user_locale = get_user_locale();
-$categories = get_categories();
-?>
-
 <div class="category-wrapper">
     <h3>Categories</h3>
 
     <ul>
-        <?php foreach ($categories as $category): ?>
-            <li>
-                <a href="<?php echo get_category_link($category->term_id) ?>">
-                    <?php echo $category->cat_name ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
+        <?php
+        $args = array(
+            'title_li' => __(''),
+        );
 
+        wp_list_categories($args);
+        ?>
     </ul>
 </div>
